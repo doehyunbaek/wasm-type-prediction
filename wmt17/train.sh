2fe08c357ba1eb68197956350f1f7f84716e9327
Doehyun Baek <doehyunbaek@gmail.com>
2024-12-02 14:06:30 +0900

    adapt wmt17

--- wmt17/train.sh
+++ wmt17/train.sh
@@ -1,2 +1,2 @@
-onmt_build_vocab --config /home/project/wasm-type-prediction/wmt17/config.yaml --n_sample=-1 --num_threads=16 --overwrite
-onmt_train -config /home/project/wasm-type-prediction/wmt17/config.yaml
+python ~/OpenNMT-py/build_vocab.py  --config /home/project/wasm-type-prediction/wmt17/config.yaml --n_sample=-1 --num_threads=16 --overwrite
+python ~/OpenNMT-py/train.py -config /home/project/wasm-type-prediction/wmt17/config.yaml
