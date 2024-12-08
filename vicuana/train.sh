f8f01273de89ed9c94787f843249248e0b886d37
Doehyun Baek <doehyunbaek@gmail.com>
2024-12-08 16:02:23 +0900

    delete vicuana

--- vicuana/train.sh
+++
@@ -1,2 +0,0 @@
-onmt_build_vocab --config /home/project/wasm-type-prediction/vicuana/config.yaml --n_sample=-1 --num_threads=16 --overwrite
-onmt_train -config /home/project/wasm-type-prediction/vicuana/config.yaml
